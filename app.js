aa202f1425fccda820031cc39fc23a939a2215ba
sonsolyom <nagydsolyom@gmail.com>
2021-01-02 19:28:19 +0100

    Adding validation and form alert to the project

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 //Book Constructor
 function Book(title, author, isbn){
+  
   this.title = title;
   this.author = author;
   this.isbn = isbn;
 }
 
 //UI Constructor
-function UI() {
-  
-}
+function UI() {}
 
+//UI Add book
 UI.prototype.addBookToList = function(book){
   
   const list = document.getElementById('book-list');
@@ -27,13 +27,36 @@ UI.prototype.addBookToList = function(book){
   list.appendChild(row);
 }
 
+//UI Show alert
+UI.prototype.showAlert = function(message, className){
+
+  //Create div
+  const div = document.createElement('div');
+  //Add classes
+  div.className = `alert ${className}`;
+  //Add text
+  div.appendChild(document.createTextNode(message));
+  //Get parent
+  const container = document.querySelector('.container');
+  //Get form
+  const form = document.querySelector('#book-form');
+  //Insert alert
+  container.insertBefore(div, form);
+
+  //Timeout after 3 seconds
+  setTimeout(()=> {
+    document.querySelector('.alert').remove();
+  }, 3000);
+}
+
 //UI Clear fields
 UI.prototype.clearFields = function() {
+  
   document.getElementById('title').value = '';
   document.getElementById('author').value = '';
   document.getElementById('isbn').value = '';
-
 }
+
 
 //Event listeners
 document.getElementById('book-form').addEventListener('submit', (e)=>{
@@ -41,19 +64,27 @@ document.getElementById('book-form').addEventListener('submit', (e)=>{
   //Get form values
   const title = document.getElementById('title').value,
         author = document.getElementById('author').value, 
-        isbn = document.getElementById('isbn').value;
-        
+        isbn = document.getElementById('isbn').value;      
   //Instantiate book
   const book = new Book(title, author, isbn);
-
   //Instantiate UI
   const ui = new UI();
 
-  //Add book to list
-  ui.addBookToList(book);
-  
-  //Clear fields
-  ui.clearFields();
+  //Validation
+  if(title === '' || author === '' || isbn === ''){
+    
+    //Error alert
+    ui.showAlert('Please fill in all fields', 'error');
+  } else {
 
+    //Add book to list
+    ui.addBookToList(book);
+    //Show success
+    ui.showAlert('Book Added', 'success');
+    //Clear fields
+    ui.clearFields();
+  }
+  
   e.preventDefault();
 })
+
