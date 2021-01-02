29e35fb3cefd0ad0b40185a14e3ee3b795b3a7de
sonsolyom <nagydsolyom@gmail.com>
2021-01-02 19:04:18 +0100

    Uploading first part of an OOP project in VanillaJS

---
+++ app.js
@@ -0,0 +1,59 @@
+//Book Constructor
+function Book(title, author, isbn){
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+//UI Constructor
+function UI() {
+  
+}
+
+UI.prototype.addBookToList = function(book){
+  
+  const list = document.getElementById('book-list');
+
+  //Create tr element
+  const row = document.createElement('tr');
+  //Insert cols
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+  `
+
+  list.appendChild(row);
+}
+
+//UI Clear fields
+UI.prototype.clearFields = function() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+
+}
+
+//Event listeners
+document.getElementById('book-form').addEventListener('submit', (e)=>{
+  
+  //Get form values
+  const title = document.getElementById('title').value,
+        author = document.getElementById('author').value, 
+        isbn = document.getElementById('isbn').value;
+        
+  //Instantiate book
+  const book = new Book(title, author, isbn);
+
+  //Instantiate UI
+  const ui = new UI();
+
+  //Add book to list
+  ui.addBookToList(book);
+  
+  //Clear fields
+  ui.clearFields();
+
+  e.preventDefault();
+})
